ea8278fc7435c09711dd7ff3c53cf1320afd2d18
Maximo Piva <maximo.piva@gmail.com>
2017-04-17 11:08:30 -0300

    Add project files.

---
+++ Goova.Plexo.Client.LocalProxy.Library/Callback.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+using Goova.Plexo.Client.LocalProxy.Logging;
+using RestSharp;
+// ReSharper disable InconsistentNaming
+
+namespace Goova.Plexo.Client.LocalProxy
+{
+    internal class Callback : ICallback //This will be loaded/injected by reflection
+    {
+        private readonly WSDLCallback _wsdlclient;
+        private readonly RestClient _restclient;
+
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+
+        public Callback()
+        {
+            if (Properties.Settings.Default.CallbackType == 0) //webservice based callback
+            {
+                _wsdlclient = WSDLCallback.Create();
+                _restclient = null;
+            }
+            else //Post based callback
+            {
+                _wsdlclient = null;
+                _restclient=new RestClient(Properties.Settings.Default.CallbackUrl);
+            }
+
+        }
+
+        public async Task<ClientResponse> Instrument(IntrumentCallback instrument)
+        {
+            if (_wsdlclient != null)
+            {
+                return await _wsdlclient.Instrument(instrument);
+            }
+            if (_restclient != null)
+            {
+                RestRequest req=new RestRequest(Method.POST);
+                req.AddJsonBody(instrument);
+                req.RequestFormat=DataFormat.Json;
+                IRestResponse<ClientResponse> resp = await _restclient.ExecuteTaskAsync<ClientResponse>(req);
+                if (resp.Data == null)
+                    return new ClientResponse {ErrorMessage = "Error executing callback: " + (resp.ErrorMessage ?? "Unknown Error"), ResultCode = ResultCodes.ClientServerError};
+                return resp.Data;
+            }
+            return new ClientResponse { ErrorMessage = "Error executing callback, callback is not configured properly", ResultCode = ResultCodes.SystemError };
+        }
+
+
+    }
+
+    public class WSDLCallback : ClientBase<ICallback>, ICallback
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+        private WSDLCallback(ServiceEndpoint enp) : base(enp)
+        {
+        }
+        public Task<ClientResponse> Instrument(IntrumentCallback instrument)
+        {
+            return Channel.Instrument(instrument);
+        }
+        public static WSDLCallback Create()
+        {
+
+            try
+            {
+                int timeout = Properties.Settings.Default.Timeout;
+                WebHttpBinding binding = new WebHttpBinding();
+                binding.OpenTimeout = TimeSpan.FromSeconds(timeout);
+                binding.CloseTimeout = TimeSpan.FromSeconds(timeout);
+                binding.SendTimeout = TimeSpan.FromSeconds(timeout);
+                binding.ReceiveTimeout = TimeSpan.FromSeconds(timeout);
+                if (Properties.Settings.Default.CallbackUrl.StartsWith("https"))
+                    binding.Security.Mode = WebHttpSecurityMode.Transport;
+
+                ServiceEndpoint svc = new ServiceEndpoint(ContractDescription.GetContract(typeof(ICallback)),
+                    binding, new EndpointAddress(Properties.Settings.Default.CallbackUrl));
+                WebHttpBehavior behavior = new WebHttpBehavior
+                {
+                    DefaultBodyStyle = WebMessageBodyStyle.Bare,
+                    DefaultOutgoingRequestFormat = WebMessageFormat.Json,
+                    DefaultOutgoingResponseFormat = WebMessageFormat.Json,
+                    HelpEnabled = true
+                };
+                svc.Behaviors.Add(behavior);
+                return new WSDLCallback(svc);
+            }
+            catch (Exception e)
+            {
+                Logger.ErrorException("Unable to create WSDLCallbackClient", e);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Goova.Plexo.Client.LocalProxy.Library/PaymentGateway.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using System.Web;
+using Goova.Plexo.Client.LocalProxy.Logging;
+using Goova.Plexo.Client.SDK;
+
+namespace Goova.Plexo.Client.LocalProxy
+{
+    public class PaymentGateway : IPaymentGateway
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        private readonly PaymentGatewayClient _cl = PaymentGatewayClientFactory.GetClient(Properties.Settings.Default.ClientName);
+
+        public async Task<ServerResponse<string>> Authorize(Authorization authorization)
+        {
+            return await OnlyRunOnIntranet(() => _cl.Authorize(authorization));
+        }
+
+        public async Task<ServerResponse<List<IssuerInfo>>> GetSupportedIssuers()
+        {
+            return await OnlyRunOnIntranet(() => _cl.GetSupportedIssuers());
+        }
+
+        public async Task<ServerResponse<Transaction>> Purchase(PaymentRequest payment)
+        {
+            return await OnlyRunOnIntranet(() => _cl.Purchase(payment));
+        }
+
+        public async Task<ServerResponse<Transaction>> Cancel(CancelRequest payment)
+        {
+            return await OnlyRunOnIntranet(() => _cl.Cancel(payment));
+        }
+
+        private async Task<ServerResponse<T>> OnlyRunOnIntranet<T>(Func<Task<ServerResponse<T>>> func)
+        {
+            try
+            {
+                string ip = null;
+                MessageProperties prop = OperationContext.Current?.IncomingMessageProperties;
+                if (prop != null)
+                {
+                    if (prop.ContainsKey(RemoteEndpointMessageProperty.Name))
+                    {
+                        RemoteEndpointMessageProperty endpoint = prop[RemoteEndpointMessageProperty.Name] as RemoteEndpointMessageProperty;
+                        if (endpoint != null)
+                            ip = endpoint.Address;
+                    }
+                }
+                if (ip == null)
+                    ip = HttpContext.Current.Request.UserHostAddress;
+                if (ip == null)
+                    return new ServerResponse<T> { ErrorMessage = "Unable to get incoming ip", ResultCode = ResultCodes.SystemError };
+                if (IsIntranet(ip))
+                    return await func();
+                return new ServerResponse<T> { ErrorMessage = "Not allowed to request from Internet", ResultCode = ResultCodes.Forbidden };
+            }
+            catch (Exception e)
+            {
+                Logger.ErrorException("System Error",e);
+                return new ServerResponse<T> {ErrorMessage = "System Error, see error log for more detailes", ResultCode = ResultCodes.SystemError};
+            }
+
+        }
+
+        private bool IsIntranet(string ip)
+        {
+            if (ip.StartsWith("172."))
+            {
+                string[] parts = ip.Split('.');
+                int sec;
+                if (parts.Length > 1 && int.TryParse(parts[1], out sec))
+                {
+                    if (sec >= 16 && sec <= 32)
+                        return true;
+                }
+            }
+            if (ip.StartsWith("127.0.0.") || ip == "::1" || ip.StartsWith("10.") || ip.StartsWith("192.168."))
+                return true;
+            if (ip.Contains(":") && (ip.StartsWith("fc") || ip.StartsWith("fd")))
+                return true;
+            return false;
+        }
+    }
+}
